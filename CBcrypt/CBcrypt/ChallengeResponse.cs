a49e8827b5aa9b6e88fb1096962ef02f59cef4d2
Edward Ned Harvey <edward.harvey@conceptblossom.com>
2015-07-06 10:21:33 -0400

    Added ChallengeResponse.GetObsoleteChallengeResponse, for client compatibility with old servers

--- CBcrypt/CBcrypt/ChallengeResponse.cs
+++ CBcrypt/CBcrypt/ChallengeResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace Org.CBCrypt
+﻿using System;
+
+namespace Org.CBCrypt
 {
 	public class ChallengeResponse
 	{
@@ -14,6 +16,20 @@
 			this.PublicKeyDerEncoded = clientKey.GetPublicKeyDerEncoded();
 			byte[] sharedSecret = clientKey.GetSharedSecret(ChallengeFromServer.ServerPublicKeyDerEncoded);
 			this.ChallengeResponseBytes = Challenge.GenerateChallengeResponseBytes(ChallengeFromServer.ChallengeBytes,sharedSecret);
+		}
+
+		/// <summary>
+		/// GetObsoleteChallengeResponse exists solely for backward compatibility, for clients to connect to servers running
+		/// CBCrypt 1.0.5.0 or lower.
+		/// </summary>
+		[Obsolete]
+		public static ChallengeResponse GetObsoleteChallengeResponse(CBCryptKey clientKey, Challenge challengeFromServer)
+		{
+			var response = new ChallengeResponse();
+			response.PublicKeyDerEncoded = clientKey.GetPublicKeyDerEncoded();
+			byte[] obsoleteSharedSecret = clientKey.GetObsoleteSharedSecret(challengeFromServer.ServerPublicKeyDerEncoded);
+			response.ChallengeResponseBytes = Challenge.GenerateChallengeResponseBytes(challengeFromServer.ChallengeBytes, obsoleteSharedSecret);
+			return response;
 		}
 	}
 }
