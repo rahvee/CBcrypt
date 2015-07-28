03542e4da294ef09e3dbc8ccf05e32f64e621cd0
Edward Ned Harvey <edward.harvey@conceptblossom.com>
2015-07-28 18:31:07 -0400

    Removed obsolete components

--- CBcrypt/CBcrypt/ChallengeResponse.cs
+++ CBcrypt/CBcrypt/ChallengeResponse.cs
@@ -17,19 +17,5 @@ namespace Org.CBCrypt
 			byte[] sharedSecret = clientKey.GetSharedSecret(ChallengeFromServer.ServerPublicKeyDerEncoded);
 			this.ChallengeResponseBytes = Challenge.GenerateChallengeResponseBytes(ChallengeFromServer.ChallengeBytes,sharedSecret);
 		}
-
-		/// <summary>
-		/// GetObsoleteChallengeResponse exists solely for backward compatibility, for clients to connect to servers running
-		/// CBCrypt 1.0.5.0 or lower.
-		/// </summary>
-		[Obsolete]
-		public static ChallengeResponse GetObsoleteChallengeResponse(CBCryptKey clientKey, Challenge challengeFromServer)
-		{
-			var response = new ChallengeResponse();
-			response.PublicKeyDerEncoded = clientKey.GetPublicKeyDerEncoded();
-			byte[] obsoleteSharedSecret = clientKey.GetObsoleteSharedSecret(challengeFromServer.ServerPublicKeyDerEncoded);
-			response.ChallengeResponseBytes = Challenge.GenerateChallengeResponseBytes(challengeFromServer.ChallengeBytes, obsoleteSharedSecret);
-			return response;
-		}
 	}
 }
